5f1f3a744be87d813bf70cfd42e3e604b86a4f58
Bryan Goss <bcgoss@gmail.com>
2016-09-05 15:36:18 -0600

    Refactored Partial for forms

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,5 @@
 class ArtistsController < ApplicationController
+  before_action :set_artist, only: [:show, :edit, :update, :destroy]
   def index
     @artists = Artist.all
   end
@@ -17,15 +18,12 @@ class ArtistsController < ApplicationController
   end
 
   def show
-    @artist = Artist.find(params[:id])
   end
 
   def edit
-    @artist = Artist.find(params[:id])
   end
 
   def update
-    @artist = Artist.find(params[:id])
     if @artist.update_attributes(artist_params)
       redirect_to @artist
     else
@@ -34,10 +32,14 @@ class ArtistsController < ApplicationController
   end
 
   def destroy
-    Artist.destroy(params[:id])
+    @artist.destroy
     redirect_to artists_path
   end
   private
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
+
   def artist_params
     params.require(:artist).permit(:name, :image_path)
   end
